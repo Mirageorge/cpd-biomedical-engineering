6c1a075c279de4fcb2585da5623b475529c417d9
Miranda George <93601807+Mirageorge@users.noreply.github.com>
2026-01-29 22:04:36 +0000

    python basics and notes

---
+++ programming/pythonbasics.py
@@ -0,0 +1,19 @@
+# Variables
+name = "Miranda"
+age = 30
+
+# Math
+
+
+#List
+
+
+#Dictionary
+patient = {
+    "name" : "John",
+    "age": 50,
+    "condition" : "Hypoglycemia"
+}
+
+print(name)
+print(patient)
